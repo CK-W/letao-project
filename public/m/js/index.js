22fe144eccdf3f8d7e56b120dec403470345e65c
wulizhou <1023841932@qq.com>
2018-06-07 20:30:29 +0800

    2017-6-7 更改了轮播图圆点的样式

--- public/m/js/index.js
+++ public/m/js/index.js
@@ -4,8 +4,8 @@ gallery.slider({
   interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
 });
 
-var sildes = document.querySelectorAll(".mui-indicator");
-for(var i =0;i<sildes.length;i++){
-    sildes[i].style.width = 15+"px";
-    sildes[i].style.height = 15+"px";
-}
+// var sildes = document.querySelectorAll(".mui-indicator");
+// for(var i =0;i<sildes.length;i++){
+//     sildes[i].style.width = 15+"px";
+//     sildes[i].style.height = 15+"px";
+// }
